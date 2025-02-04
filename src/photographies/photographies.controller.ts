65594774b8c260985d34ad7419068bb843509062
kevocodes <00116120@uca.edu.sv>
2025-02-04 01:01:35 -0600

    fix: resolve vercel errors :bug:

--- src/photographies/photographies.controller.ts
+++ src/photographies/photographies.controller.ts
@@ -10,9 +10,9 @@ import {
 import { PhotographiesService } from './photographies.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
-import { ApiResponse } from 'src/common/types/response.type';
-import { getParseImagePipe } from 'src/common/utils/get-parse-file-pipe';
-import { MongoIdPipe } from 'src/common/pipes/mongo-id.pipe';
+import { ApiResponse } from '../common/types/response.type';
+import { getParseImagePipe } from '../common/utils/get-parse-file-pipe';
+import { MongoIdPipe } from '../common/pipes/mongo-id.pipe';
 
 @Controller('photographies')
 export class PhotographiesController {
