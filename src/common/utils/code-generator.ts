3f4bd5d49971912dbc61b847faa3194b5d4b84d9
kevocodes <00116120@uca.edu.sv>
2025-02-04 01:04:35 -0600

    fix: resolve vercel errors :bug:

--- src/common/utils/code-generator.ts
+++ src/common/utils/code-generator.ts
@@ -1,5 +1,5 @@
 export const generateRandomAlphanumericCode = (length: number = 6) => {
-  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let code = '';
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
